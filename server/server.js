162deb67de7316a060037c0ce334db4e6a98ee3f
Max W <mwinter@wesleyan.edu>
2022-02-27 22:03:08 -0600

    client side files added

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require('express');
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
@@ -26,6 +27,14 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
