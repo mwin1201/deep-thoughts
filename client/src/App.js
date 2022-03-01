6ef4ebaa7be47717b82abe771e06c9e0e6b35c4a
Max W <mwinter@wesleyan.edu>
2022-03-01 16:14:27 -0600

    added login and signin features

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import NoMatch from "./pages/NoMatch";
 import SingleThought from "./pages/SingleThought";
 import Profile from "./pages/Profile";
 import Signup from "./pages/Signup";
+import { setContext } from "@apollo/client/link/context";
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -16,8 +17,18 @@ const httpLink = createHttpLink({
   uri: "/graphql",
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
